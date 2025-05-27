4e5af95568cd8a5a5ad6d84db057bbfb1b899fdc
yhs-coder <yhs6666@outlook.com>
2025-05-27 23:50:24 +0800

    测试线程池基本功能

--- components/threadpool/main.cpp
+++ components/threadpool/main.cpp
@@ -1,12 +1,33 @@
-#include <iostream>
-#include "threadpool.h"
 #include <chrono>
+#include <iostream>
+
+#include "threadpool.h"
+
+class MyTask : public Task {
+public:
+    void Run() override {
+        std::cout << "begin..." << std::endl;
+        std::cout << "thread id: " << std::this_thread::get_id() << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+        std::cout << "end..." << std::endl;
+    }
+};
 
 int main() {
     ThreadPool pool;
-    pool.Start(6);
-
-    std::this_thread::sleep_for(std::chrono::seconds(5));
+    pool.Start(4);
+    pool.SubmitTask(std::make_shared<MyTask>());
+    pool.SubmitTask(std::make_shared<MyTask>());
+    pool.SubmitTask(std::make_shared<MyTask>());
+    pool.SubmitTask(std::make_shared<MyTask>());
+    pool.SubmitTask(std::make_shared<MyTask>());
+    pool.SubmitTask(std::make_shared<MyTask>());
+    pool.SubmitTask(std::make_shared<MyTask>());
+    pool.SubmitTask(std::make_shared<MyTask>());
+    pool.SubmitTask(std::make_shared<MyTask>());
+    pool.SubmitTask(std::make_shared<MyTask>());
+    pool.SubmitTask(std::make_shared<MyTask>());
+    //    std::this_thread::sleep_for(std::chrono::seconds(5));
+    getchar();
     return 0;
 }
-
